fe1cdaec74a3c404ac227ff346a3bf99ef1a59bc
rhernand <rhernand@student.42malaga.com>
2024-04-20 18:09:46 +0200

    menos atoi

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 20:32:08 by rhernand          #+#    #+#             */
-/*   Updated: 2024/04/15 23:21:16 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/04/20 17:40:11 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ Si size <= dst, la función devuelve size + src.
 Si el size >= que dst, la función devuelve dst + src.
 
 #include <string.h>
-#include <stdio.h>
-*/
+#include <stdio.h>*/
 
 #include "libft.h"
 
-t_size	ft_strlcat(char *dst, char *src, t_size size)
+t_size	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	t_size	dst_size;
 	t_size	dst_count;
@@ -30,14 +29,10 @@ t_size	ft_strlcat(char *dst, char *src, t_size size)
 	dst_size = ft_strlen(dst);
 	dst_count = dst_size;
 	src_count = 0;
-	if (dst_count >= size)
-		return (size + ft_strlen(src));
-	while (dst_count < size - 1)
-	{
-		dst[dst_count] = src[src_count];
-		dst_count ++;
-		src_count ++;
-	}
+	if (dst_count >= dstsize)
+		return (dstsize + ft_strlen(src));
+	while (dst_count < dstsize - 1 && src[src_count])
+		dst[dst_count++] = src[src_count++];
 	dst[dst_count] = '\0';
 	return (ft_strlen(src) + dst_size);
 }
@@ -57,5 +52,5 @@ t_size	ft_strlcat(char *dst, char *src, t_size size)
 	ft_strlcpy(buffer_ft, dst, 12);
 	printf("ft_strlcat = %lu\n", ft_strlcat(buffer_ft, src, 12));
 	printf("dst strlcat = %s\n", buffer_ft);
-}
-*/
+	strlcat("pqrstuvwxyz", "abcd", 20);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 10:37:42 by rhernand          #+#    #+#             */
-/*   Updated: 2024/04/19 15:52:04 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/04/20 17:51:22 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int				ft_isascii(int c);
 int				ft_isprint(int c);
 t_size			ft_strlen(const char *s);
 t_size			ft_strlcpy(char *dst, char *src, t_size dstsize);
-t_size			ft_strlcat(char *dst, char *src, t_size dstsize);
+t_size			ft_strlcat(char *dst, const char *src, size_t dstsize);
 char			*ft_strnstr(char *haystack, char *needle, t_size len);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
@@ -37,5 +37,7 @@ void			*ft_memchr(const void *s, int c, size_t n);
 void			*ft_memset(void *b, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
+int				ft_memcmp(void *s1, void *s2, size_t n);
 
 #endif
