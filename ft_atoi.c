5cec277510dfd12f994285ecf8baf5fdd2f6ba8b
rhernand <rhernand@student.42malaga.com>
2024-04-20 19:34:33 +0200

    first block

---
+++ ft_atoi.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 18:10:21 by rhernand          #+#    #+#             */
+/*   Updated: 2024/04/20 19:32:46 by rhernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*if " " keep moving
+if "-" minus sign
+if "--" return 0
+if other. return 0 */
+
+/* #include <stdio.h>
+#include <stdlib.h> */
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	i;
+	int	n;
+
+	sign = 1;
+	n = 0;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\v' || str[i] == '\f'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + (str[i] - 48);
+		i++;
+	}
+	return (n * sign);
+}
+
+/* int	main(void)
+{
+	char str[] = "  \r 459822294 oo";
+
+	printf("atoi result = %d\n", atoi(str));
+	printf("ft_atoi result = %d\n", ft_atoi(str));
+}
+ */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 10:37:42 by rhernand          #+#    #+#             */
-/*   Updated: 2024/04/20 17:51:22 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/04/20 18:46:29 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 int				ft_memcmp(void *s1, void *s2, size_t n);
+int				ft_atoi(const char *str);
 
 #endif
