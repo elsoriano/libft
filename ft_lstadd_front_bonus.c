7cfb03535f35e9f18e46c3c5f5c1ee6b048cbb50
rhernand <rhernand@student.42malaga.com>
2024-05-07 09:12:33 +0200

    protections for ft_lstadd_front

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rodrigo <rodrigo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 07:50:48 by rodrigo           #+#    #+#             */
-/*   Updated: 2024/05/05 09:31:45 by rodrigo          ###   ########.fr       */
+/*   Updated: 2024/05/07 09:09:35 by rodrigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list	**lst, t_list *new)
 {
+	if (!new || !lst)
+		return;
 	new->next = *lst;
 	*lst = new;
 }
