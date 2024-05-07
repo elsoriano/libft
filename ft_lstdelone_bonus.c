a684cdd9e99276311fe2db7e2989155f127d3b88
rhernand <rhernand@student.42malaga.com>
2024-05-07 10:32:28 +0200

    lstdelone and lstadd_front*

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -1,21 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rodrigo <rodrigo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/05 07:50:48 by rodrigo           #+#    #+#             */
-/*   Updated: 2024/05/07 09:44:24 by rodrigo          ###   ########.fr       */
+/*   Created: 2024/05/07 09:46:04 by rodrigo           #+#    #+#             */
+/*   Updated: 2024/05/07 10:25:46 by rodrigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list	**lst, t_list *new)
+void	ft_lstdelone(t_list *lst, void (*del) (void*))
 {
-	if (!new || !lst)
+	if (!lst)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	del(lst->content);
+	free (lst);
+	lst = NULL;
+	return ;
 }
+
+/* void	ft_del(void *content)
+{
+	return ;
+}
+
+int	main(void)
+{
+	t_list *lst;
+
+	lst = malloc(sizeof(t_list));
+	if (!lst)
+		return (1);
+	lst->content = "hello";
+	lst->next = NULL;
+	ft_lstdelone(lst, ft_del);
+	printf("deleted node = %s\n", (char *) lst->content);
+} */
